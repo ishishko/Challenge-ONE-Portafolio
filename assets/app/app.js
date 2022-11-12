db9054ae6271b95a35d9279dca5a0781ce330c06
Ishishko <shishkoignacio@gmail.com>
2022-11-12 18:31:36 -0300

    ONE Portafolio V1.0

--- assets/app/app.js
+++ assets/app/app.js
@@ -7,7 +7,32 @@ window.onload = () => {
 
 function eventos() {
   const html = document.querySelector("html");
-  console.log(html.clientWidth);
   cargaTerminada();
   navFixed();
+}
+
+const $form = document.querySelector(".contacto--formulario");
+$form.addEventListener("submit", enviar);
+
+async function enviar(e) {
+  e.preventDefault();
+  const form = new FormData(this);
+  console.log(form);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    const btnEnviar = document.querySelector(".contacto--button");
+    this.reset();
+    btnEnviar.innerHTML = "Mensaje Enviado";
+    btnEnviar.classList.add("contacto--button__ok");
+    setTimeout(() => {
+      btnEnviar.classList.remove("contacto--button__ok");
+      btnEnviar.innerHTML = "Enviar";
+    }, 2500);
+  }
 }
